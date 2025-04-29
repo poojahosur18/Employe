074eb73dc79f21c23289df4fc811f1ad4b046984
poojahosur18 <poojahosur18gmail.com>
2025-04-29 11:13:44 +0530

    created

--- Employee.java
+++ Employee.java
@@ -2,21 +2,25 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String location;
 
-    public Employee(String name, int id, double salary) {
+    public Employee(String name, int id, double salary,String location) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.location = location;
     }
 
     public void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
+        System.out.println("location: " + location);
+
     }
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("Tanuja", 101, 50000.0);
+        Employee emp1 = new Employee("Tanuja", 101, 50000.0,"jagadal");
         emp1.displayInfo();
 
 }
